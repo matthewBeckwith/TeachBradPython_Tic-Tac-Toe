2dce03a56480ec90e97ad5c91597ce697300eea4
Matthew Beckwith <kodahScripts@gmail.com>
2017-07-09 14:57:30 -0400

    Basic winning and loosing added.. AI isn't smart yet.

--- tictactoe_gameboard.py
+++ tictactoe_gameboard.py
@@ -9,43 +9,45 @@
 import random
 
 #make a board that the code can check
-board = [
-    0,0,0,
-    0,0,0,
-    0,0,0
-]
+def initGame():
+    board = [
+        0,0,0,
+        0,0,0,
+        0,0,0
+    ]
+    drawBoard(board)
+
 
 #we reuse this code a lot so we make it a function
-def choose():
+def choose(board):
     choice = int(input("Choose a Box (0-8)"))
 
     if(choice >= 0):
         if(choice <= 8):
             if(board[choice] > 0):
                 print("This has already been picked.... Please pick another")
-                choose()
+                choose(board)
             else:
                 board[choice] = 1
-                computerChoice()
+                computerChoice(board)
         else:
-            choice()
+            choose(board)
     else:
-        choice()
+        choose(board)
     
 
 #Give the player an Enemy
-def computerChoice():
+def computerChoice(board):
     choice = random.randint(-1,9)
 
     if(board[choice] > 0):
-        computerChoice()
+        computerChoice(board)
     else:
         board[choice] = 2
-        drawBoard()
+        drawBoard(board)
 
 #Handles showing the board to the user
-def drawBoard():
-    print("\n" * 100)
+def drawBoard(board):
     cell = []
 
     for i in range(0,9):
@@ -55,7 +57,8 @@ def drawBoard():
             cell.append('O')
         else:
             cell.append(' ')
-
+            
+    print("\n" * 100)
     print("""
     TICK-TAC-TOE!
 ---------------------
@@ -72,6 +75,51 @@ def drawBoard():
     print("  " + cell[6] + "  |  " + cell[7] + "  |  " + cell[8])
     print("     |     |")
     
-    choose()
+    checkForWinner(board)
 
-drawBoard()
+#Check if a Player Won
+def checkForWinner(board):
+    if(board[0] == 1 and board[1] == 1 and board[2] == 1 or
+       board[3] == 1 and board[4] == 1 and board[5] == 1 or
+       board[6] == 1 and board[7] == 1 and board[8] == 1 or
+       board[0] == 1 and board[3] == 1 and board[6] == 1 or
+       board[1] == 1 and board[4] == 1 and board[7] == 1 or
+       board[2] == 1 and board[5] == 1 and board[8] == 1 or
+       board[0] == 1 and board[4] == 1 and board[8] == 1 or
+       board[2] == 1 and board[4] == 1 and board[6] == 1):
+        print("\n" * 100)
+        print("You Win!")
+        playAgain()
+    elif(board[0] == 2 and board[1] == 2 and board[2] == 2 or
+       board[3] == 2 and board[4] == 2 and board[5] == 2 or
+       board[6] == 2 and board[7] == 2 and board[8] == 2 or
+       board[0] == 2 and board[3] == 2 and board[6] == 2 or
+       board[1] == 2 and board[4] == 2 and board[7] == 2 or
+       board[2] == 2 and board[5] == 2 and board[8] == 2 or
+       board[0] == 2 and board[4] == 2 and board[8] == 2 or
+       board[2] == 2 and board[4] == 2 and board[6] == 2):
+        print("\n" * 100)
+        print("You Loose!")
+        playAgain()
+    else:
+        choose(board)
+
+
+#ask if the player wants to play again
+def playAgain():
+    choice = int(input("""
+        Play Again?
+
+        1 - Yes
+        2 - No
+    """))
+
+    if(choice == 1):
+        initGame()
+    elif(choice == 2):
+        quit()
+    else:
+        playAgain()
+
+#init the game
+initGame()
